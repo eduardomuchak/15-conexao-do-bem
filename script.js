ea28e450e288b0a2a3f8bb2a498d6c73683b3396
Eduardo Muchak <eduardomuchak@gmail.com>
2022-02-11 19:12:41 -0300

    Adiciona dropdown de estados
    Co-authored-by: VINICIUS DE PAULA MARTINS DOS ANJOS <vinidipaula92@users.noreply.github.com>
    Co-authored-by: Rodrigo de Castro <rodrigo-de-castro@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const botaoCadastrar = document.querySelector('.btn-success');
+const botaoLimpar = document.querySelector('#botao-limpar');
+const listaOngs = document.querySelector('.institution-list')
+
 let map;
 
 function initMap() {
@@ -27,3 +31,90 @@ const marker = new google.maps.Marker({
     //icon: caminho_da_imagem
 });
 }; 
+
+let rua = document.querySelector('#rua');
+let numeroPredial = document.querySelector('#numero');
+let bairro = document.querySelector('#bairro');
+let cidade = document.querySelector('#cidade');
+let estado = document.querySelector('#estado');
+let nomeDaOng = document.querySelector('#nome-da-ong');
+
+function limpa_formulário_cep() {
+  //Limpa valores do formulário de cep.
+  rua.value = '';
+  bairro.value = '';
+  cidade.value = '';
+  estado.value = '';
+};
+
+function meu_callback(conteudo) {
+  if (!("erro" in conteudo)) {
+    //Atualiza os campos com os valores.
+    rua.value = (conteudo.logradouro);
+    bairro.value = (conteudo.bairro);
+    cidade.value = (conteudo.localidade);
+    estado.value = (conteudo.uf);
+  } //end if.
+  else {
+    //CEP não Encontrado.
+    limpa_formulário_cep();
+    alert("CEP não encontrado.");
+  }
+};
+
+function pesquisacep(valor) {
+  //Nova variável "cep" somente com dígitos.
+  let cep = valor.replace(/\D/g, '');
+
+  //Verifica se campo cep possui valor informado.
+  if (cep != "") {
+
+    //Expressão regular para validar o CEP.
+    let validacep = /^[0-9]{8}$/;
+
+    //Valida o formato do CEP.
+    if(validacep.test(cep)) {
+
+        //Preenche os campos com "..." enquanto consulta webservice.
+        rua.value = "...";
+        bairro.value = "...";
+        cidade.value = "...";
+        estado.value = "...";
+
+        //Cria um elemento javascript.
+        let script = document.createElement('script');
+
+        //Sincroniza com o callback.
+        script.src = `https://viacep.com.br/ws/${cep}/json/?callback=meu_callback`;
+
+        //Insere script no documento e carrega o conteúdo.
+        document.body.appendChild(script);
+
+    } //end if.
+    else {
+        //cep é inválido.
+        limpa_formulário_cep();
+        alert("Formato de CEP inválido.");
+    }
+  } //end if.
+  else {
+    //cep sem valor, limpa formulário.
+    limpa_formulário_cep();
+  }
+};
+
+function createElementP(valor) {
+  const info = document.createElement('p');
+  info.innerText = valor;
+  return info 
+}
+
+function createDiv() {
+  const item = document.createElement('li');
+  item.appendChild(createElementP(nomeDaOng.value));
+  item.appendChild(createElementP(cidade.value));
+  item.appendChild(createElementP(estado.value));
+  listaOngs.appendChild(item);  
+}
+
+botaoCadastrar.addEventListener('click', createDiv);
