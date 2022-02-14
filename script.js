8df7cc39e7463a5859994f8c6bbc13f7d6ae0c08
Eduardo Muchak <eduardomuchak@gmail.com>
2022-02-14 16:52:55 -0300

    Adiciona ajuste de altura das imagens ong parceira

--- script.js
+++ script.js
@@ -102,14 +102,8 @@ function getOngInfo() {
   createH5.innerHTML = `${nomeDaOng.value.toUpperCase()}`;
   createLi.appendChild(createSpan);
   createSpan.innerText = `ENDEREÇO: ${rua.value.toUpperCase()} - ${numeroPredial.value}, ${bairro.value.toUpperCase()} - ${cidade.value.toUpperCase()}/${estado.value.toUpperCase()}`;
-  return createLi; 
+  return createLi;
 };
-
-/* function getCepSave() {
-  const createSpan = document.createElement('span');
-  createSpan.classList.add(inputCep.value);
-  
-}; */
 
 async function appendOngList(event) {
   event.preventDefault();
@@ -186,8 +180,7 @@ window.onload = async () => {
     map: map,//Objeto mapa
     title: nomeDaOng.value
   });
-  })
-
-}
+  });
+};
 
 module.exports = pesquisacep;
